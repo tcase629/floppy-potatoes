b32efbaf92720aced00eb769c0828990790fe44a
Tyler Case <tcase629@yahoo.com>
2022-04-23 17:29:47 -0600

    Home page

---
+++ client/src/components/Movies/Movie.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Card = styled.div`
+  border: 1px solid #efefef;
+  background: #fff;
+  text-align: center;
+`
+const MoviePoster = styled.div`
+  width: 50px;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 10px;
+
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid #efefef;
+  }
+`
+const MovieName = styled.div`
+  padding: 20px 0 10px 0;
+`
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+
+  a {
+    color: #fff;
+    background-color: #000;
+    border-radius: 4px;
+    padding: 10px 50px;
+    border: 1px solid #000;
+    width: 100%;
+    text-decoration: none;
+  }
+`
+
+const Movie = (props) => {
+  return (
+    <Card>
+      <MoviePoster>
+        <img src={props.attributes.image_url} alt={props.attributes.name} />
+      </MoviePoster>
+      <MovieName>{props.attributes.name}</MovieName>
+      <div className="movie-score">{props.attributes.avg_score}</div>
+      <LinkWrapper>
+        <Link to={`/movies/${props.attributes.slug}`}>View Movie</Link>
+      </LinkWrapper>
+    </Card>
+  )
+}
+
+export default Movie
--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -1,8 +1,62 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import styled from 'styled-components';
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto; 
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+const SubHeader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
 
 const Movies = () => {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    axios.get('/api/movies.json')
+    .then(resp => setMovies(resp.data.data))
+    .catch(resp => console.log(resp))
+  }, [movies.length])
+
+  const grid = movies.map( i => {
+    return (
+      <Movie 
+        key={i.attributes.name}
+        attributes={i.attributes} 
+      />
+    )
+  })
+
   return (
-    <div>This is the Movies#index!</div>
+    <Home>
+      <Header>
+        <h1>Floppy Potatoes</h1>
+        <SubHeader>Movie reviews that are just...meh!</SubHeader>
+      </Header>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
   )
 }
 
